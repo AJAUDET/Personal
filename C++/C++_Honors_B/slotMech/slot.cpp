fc0f24af27f0f29a900e461a2c544aeee246bb1a
AJAUDET <150381581+AJAUDET@users.noreply.github.com>
2024-02-01 15:06:54 -0500

    Add files via upload

---
+++ C++/C++_Honors_B/slotMech/slot.cpp
@@ -0,0 +1,23 @@
+/*
+    slot.cpp by AJ Audet
+
+    making a slot machine, yk 🎰🎰🎰, the best game
+
+    using rand seems appropriate or yk i love "fixed" percentages 2% droprates(Warfrane)
+
+    make an array of possible values use it 3 times
+*/
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    // introducing randomness
+    srand(time(NULL));
+
+    // array of the symbols 
+    string symbols[] = {};
+
+    return 0;
+}
---
+++ C++/mystuff/blackJack/blackJack.cpp
@@ -0,0 +1,14 @@
+/*
+    blackJack.cpp by AJ Audet
+
+    makin blackjack
+*/
+
+#include <iostream>
+
+int main()
+{
+    srand(time(NULL));
+    
+    return 0;
+}
